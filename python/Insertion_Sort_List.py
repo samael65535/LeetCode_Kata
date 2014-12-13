055ba5f405216b9aab5f4652ea8cd1dc55d13b6a
samael <samael.65535@gmail.com>
2014-12-13 17:13:12 +0800

    cpp Insert_Sort_List

--- python/Insertion_Sort_List.py
+++ python/Insertion_Sort_List.py
@@ -1,11 +1,49 @@
+import random
+import math
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+        def __init__(self, x):
+                self.val = x
+                self.next = None
 
 class Solution:
-    # @param head, a ListNode
-    # @return a ListNode
-    def insertionSortList(self, head):
-        return 0
+        # @param head, a ListNode
+        # @return a ListNode
+        def insertionSortList(self, head):
+                if head is None:
+                        return head
+                newHead = ListNode(0)
+                newHead.next = head
+                curr = head
+                while curr.next:
+                        if curr.next.val < curr.val:
+                                pre = newHead
+                                while pre.next.val < curr.next.val:
+                                        pre = pre.next
+                                tmp = curr.next
+                                curr.next = tmp.next
+                                tmp.next = pre.next
+                                pre.next = tmp
+                        else:
+                                curr = curr.next
+                return newHead.next
+
+if __name__ == "__main__":
+        s = Solution()
+        head = ListNode(math.ceil(random.random() * 100))
+        nowNode = head
+        print nowNode.val,
+        for i in range(0, 20):
+                nowNode.next = ListNode(math.ceil(random.random() * 100))
+#		nowNode.next = ListNode(i)
+                nowNode = nowNode.next
+                print nowNode.val,
+        print
+        print "after"
+        a = []
+        head = s.insertionSortList(head)
+        while head is not None:
+                print head.val,
+                head = head.next
+        print
+        print "ok"
