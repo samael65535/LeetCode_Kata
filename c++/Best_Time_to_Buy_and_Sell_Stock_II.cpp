817d255bcb4d57df90c2b9c41d61317060bde29c
samael <samael.65535@gmail.com>
2014-12-15 22:36:06 +0800

    Best Time to Buy and Sell Stock II

--- c++/Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ c++/Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -6,6 +6,13 @@
 class Solution {
 public:
 	int maxProfit(vector<int> &prices) {
-
+	  if (prices.size() == 0) return 0;
+	  int profit = 0;
+	  for (auto i = 0; i < prices.size()-1; ++i) {
+		if (prices[i+1] - prices[i] > 0) {
+		  profit += prices[i+1] - prices[i];
+		}
+	  }
+	  return profit;
 	}
 };
--- python/Best_Time_to_Buy_and_Sell_Stock_II.py
+++ python/Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -3,7 +3,18 @@
 # Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
 class Solution:
-    # @param prices, a list of integer
-    # @return an integer
+# @param prices, a list of integer
+# @return an integer
     def maxProfit(self, prices):
-        return 0
+        if (not prices):
+            return 0
+        profit = 0
+        for i in range(0, len(prices) - 1):
+            if (prices[i + 1] - prices[i] > 0):
+                profit = (prices[i + 1] - prices[i]) + profit
+        return profit
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.maxProfix()
