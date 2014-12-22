73ef0e4bd4b4477c7fff4f6cef344e19fee0baa4
samael <samael.65535@gmail.com>
2014-12-22 23:02:10 +0800

    python 平衡二叉树

--- python/Balanced_Binary_Tree.py
+++ python/Balanced_Binary_Tree.py
@@ -4,13 +4,33 @@
 
 
 # Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     # @param root, a tree node
     # @return a boolean
     def isBalanced(self, root):
+        if root is None:
+            return True
+        if abs(self.depth(root.right) - self.depth(root.left)) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def depth(self, root):
+        if root is None:
+            return 0
+        leftDepth = self.depth(root.left)
+        rightDepth = self.depth(root.right)
+        return 1 + max(leftDepth, rightDepth)
+
+if __name__ == "__main__" :
+    root = TreeNode(1)
+    root.left = TreeNode(1)
+    root.right = TreeNode(1)
+    s = Solution()
+    print s.isBalanced(root)
+    print "hello"
