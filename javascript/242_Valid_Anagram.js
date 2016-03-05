87c8cf842f53ffaa68a438856e66b1b42b841224
samael <samael.65535@gmail.com>
2016-03-05 17:30:05 +0800

    anagram

--- javascript/242_Valid_Anagram.js
+++ javascript/242_Valid_Anagram.js
@@ -8,11 +8,14 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    var s1  = s.split("").sort(function(a, b) {
-        return a > b;
-    }).toString();
-    var s2  = t.split("").sort(function(a, b) {
-        return a > b;
-    }).toString();
-    return s1 === s2;
+    var s1  = s.split("").sort();
+    var s2  = t.split("").sort();
+    var length = s.length;
+    if (t.length !== s.length) return false;
+    for (var i = 0; i < length; i++) {
+        if (s2[i] !== s1[i]) return false
+    }
+    return true
 };
+
+console.log(isAnagram("rat", "cat"))
