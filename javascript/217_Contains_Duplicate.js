ec912319a6594e0876ee1c7c97c5bc1e8f74a999
samael <samael.65535@gmail.com>
2016-03-14 13:35:33 +0800

    Contains Duplicate

--- javascript/217_Contains_Duplicate.js
+++ javascript/217_Contains_Duplicate.js
@@ -6,5 +6,20 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-
+    if (nums.length <= 1) return false;
+    var dic = {};
+    var iterator = nums.entries();
+    var n = iterator.next();
+    while(n.done === false) {
+        var key = n.value[1];
+        if (dic[key] !== undefined) {
+            return true;
+        }
+        dic[key] = 1;
+        n = iterator.next();
+    }
+    return false
 };
+
+console.log(containsDuplicate([1,3,3,5,5,6]));
+console.log(containsDuplicate([1,1]));
