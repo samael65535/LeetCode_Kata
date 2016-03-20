5ebea87dd88ddf427227a41fa6055056031ae8e4
samael <samael.65535@gmail.com>
2016-03-20 13:09:50 +0800

    169

---
+++ javascript/169_Majority_Element.js
@@ -0,0 +1,27 @@
+/**
+ * Created by samael on 3/20/16.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    nums.sort(function(a, b) {
+        return a - b;
+    });
+    var currentElement = null;
+    var currentCount = 0;
+    for(var i = 0; i < nums.length; i ++) {
+        var v= nums[i];;
+        if (currentElement == v) currentCount ++;
+        else {
+            currentElement = v;
+            currentCount = 1;
+        }
+        if (currentCount > nums.length / 2) return currentElement;
+    }
+    return null;
+};
+
+console.log(majorityElement([47,47,72,47,72,47,79,47,12,92,13,47,47,83,33,15,18,47,47,47,47,64,47,65,47,47,47,47,70,47,47,55,47,15,60,47,47,47,47,47,46,30,58,59,47,47,47,47,47,90,64,37,20,47,100,84,47,47,47,47,47,89,47,36,47,60,47,18,47,34,47,47,47,47,47,22,47,54,30,11,47,47,86,47,55,40,49,34,19,67,16,47,36,47,41,19,80,47,47,27]));
