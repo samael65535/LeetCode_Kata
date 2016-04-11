842528b423f7d5ca08c9ba9230a3d35062fc95d2
samael <samael.65535@gmail.com>
2016-04-12 00:45:18 +0800

    318

---
+++ javascript/318_Maximum_Product_of_Word_Lengtht.js
@@ -0,0 +1,18 @@
+/**
+ * Created by samael on 4/12/16.
+ */
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maxProduct = function(words) {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for(var i=0; i<alphabet.length; i++)
+    {
+        var nextChar = alphabet.charAt(i);
+        var candi = [];
+        for (var j=0; j<words.length; j++){
+
+        }
+    }
+};
