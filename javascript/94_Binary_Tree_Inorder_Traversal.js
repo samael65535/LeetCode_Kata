459e497b8ed1f5590d06c9de9ee6f13f0f1910f0
samael <samael.65535@gmail.com>
2016-04-21 16:58:09 +0800

    94

--- javascript/94_Binary_Tree_Inorder_Traversal.js
+++ javascript/94_Binary_Tree_Inorder_Traversal.js
@@ -12,6 +12,22 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+var leetcode = require('./leetcode');
 var inorderTraversal = function(root) {
-
+    var ret = [];
+    var queue = [];
+    var r = root;
+    while(queue.length > 0 || r) {
+        if (!r) {
+            r = queue.pop();
+            ret.push(r.val);
+            r = r.right;
+        } else {
+            queue.push(r);
+            r = r.left;
+        }
+    }
+    return ret;
 };
+var root = leetcode.Tree.create([1,2,3]);
+console.log(inorderTraversal(root));
