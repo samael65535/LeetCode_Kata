cb39acb5c41eee6e75531628eb715bd891d8edcc
samael <samael.65535@gmail.com>
2016-04-23 21:44:17 +0800

    206

---
+++ javascript/206_Reverse_Linked_List.js
@@ -0,0 +1,32 @@
+/**
+ * Created by samael on 4/23/16.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var leetcode = require('./leetcode');
+var reverseList = function(head) {
+
+    if (head === null) return head;
+    var prev = null;
+    var current = head;
+    while(current) {
+        var tmp = current.next;
+        current.next = prev;
+        prev = current;
+        current = tmp;
+    }
+    return prev;
+};
+var root = leetcode.List.create([1]);
+var ret = reverseList(root);
+console.log(leetcode.List.toArray(ret));
