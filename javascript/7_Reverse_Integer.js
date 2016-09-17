aedae41aa66a179b6553397e4d941a777ae4a73e
samael <samael.65535@gmail.com>
2016-09-17 15:02:57 +0800

    7

---
+++ javascript/7_Reverse_Integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    var numStr = x.toString();
+    var max = Math.pow(2,31) - 1;
+    var min = -Math.pow(2,31);
+    var ret;
+    if (numStr[0] == "-") {
+        ret = parseInt("-" + numStr.split('').splice(1).reverse().join(''));
+    } else {
+        ret = parseInt(numStr.split('').splice(0).reverse().join(''));
+    }
+    return (ret > max || ret < min) ? 0 : ret;
+};
+console.log(reverse(-2147483648));
