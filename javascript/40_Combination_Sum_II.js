2583f1452892a5759b5c5eedbb7393b71dab6190
samael <samael.65535@gmail.com>
2016-09-24 19:49:56 +0800

    40

--- javascript/40_Combination_Sum_II.js
+++ javascript/40_Combination_Sum_II.js
@@ -3,20 +3,31 @@
  * @param {number} target
  * @return {number[][]}
  */
-var ret = [];
-var combinationSum = function(candidates, target) {
+var dict = {};
+var combinationSum2 = function(candidates, target) {
 	var arr = candidates.sort(function(a, b){return a-b;});
 	while (arr[0] > target) {
 		arr.shift();
 	}
 	var arr1 = [];
+	dict = {};
+	var ret = [];
 	dfs(arr, target, 0, arr1)
+	var t = []
+	for (var key in dict) {
+		key.split(',').forEach(function(c, i){
+			t.push(parseInt(c));
+		}, this);
+		ret.push(t.slice());			
+		t = [];
+	}
 	return ret;
 }
 
 var dfs = function(arr, target, i, arr1) {
 	if (target == 0) {
-		ret.push(arr1.slice());
+		dict[arr1.slice().join()] = 0;
+		return;
 	}
 	
 	for (var j = i; j < arr.length; j++) {
@@ -25,6 +36,6 @@ var dfs = function(arr, target, i, arr1) {
 		dfs(arr, target - arr[j], j+1, arr1);
 		arr1.pop();
 	}
-	
 }
-console.log(combinationSum([10, 1, 2, 7, 6, 1, 5], 8));
+//console.log(combinationSum([10, 1, 2, 7, 6, 1, 5], 8));
+//console.log(combinationSum([2, 3, 6, 7], 7));
