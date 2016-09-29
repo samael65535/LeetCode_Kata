8bf63805dc3cc343259a05e590378861ce8562d8
samael <samael.65535@gmail.com>
2016-09-29 14:20:33 +0800

    401

--- javascript/401_Binary_Watch.js
+++ javascript/401_Binary_Watch.js
@@ -2,6 +2,23 @@
  * @param {number} num
  * @return {string[]}
  */
+
+function getCombination(nums, c, arr, vis, start, ret) {
+	if (c == 0) {
+		ret.push(arr.slice());
+		return 1;
+	} 
+	for (var i = start; i < nums.length; i++) {
+		if (vis[i] == 0) {
+			vis[i] = 1;
+			arr.push(nums[i]);
+			getCombination(nums, c-1, arr, vis, i, ret);
+			arr.pop(nums[i]);
+			vis[i] = 0;
+		}
+	}
+}	
+
 var readBinaryWatch = function(num) {
 	var hour = 0;
 	var minute = 0;
@@ -9,29 +26,41 @@ var readBinaryWatch = function(num) {
 		[1, 2, 4, 8],
 		[1, 2, 4, 8, 16, 32]
 	];
+	var combination = [];
+	
 	var ret = [];
+	
+	for (var i = 0; i <= num; i++){
+		minute = num - i;
+		hour = i;
+		var retMinute = [];
+		var retHour = []
+		var visHour = [0, 0, 0, 0]
+		var visMinute = [0, 0, 0, 0, 0, 0];
+		getCombination(watch[0], hour, [], visHour, 0, retHour);
+		getCombination(watch[1], minute, [], visMinute, 0, retMinute);
+		
+		for (var h = 0; h < retHour.length; h++) {
+			var sumHour = 0;
+			retHour[h].forEach(function (elt, i) {
+				sumHour += elt;
+			});
+			if (sumHour > 11) continue;
+			for (var m = 0; m < retMinute.length; m++) {
+				var sumMinute = 0;
+				retMinute[m].forEach(function (elt, i) {
+					sumMinute += elt;
+				});
+				if (sumMinute > 59) continue;
+				if (sumMinute < 10) sumMinute = "0" + sumMinute;
+				ret.push(sumHour+":"+sumMinute)
 
-	// 全组合
+			}
+		}
+	}
+
 	return ret;	
 };
 
-function getCombibation(nums, c, ret, vis, start) {
-	if (c == 0) {
-		console.log(ret);
-		return ret;
-	} 
-	for (var i = start; i < nums.length; i++) {
-		if (vis[i] == 0) {
-			vis[i] = 1;
-			ret.push(nums[i]);
-			getCombibation(nums, c-1, ret, vis, i);
-			ret.pop(nums[i]);
-			vis[i] = 0;
-		}
-	}
-}	
 
-var ret = [];
-var vis = [0, 0, 0, 0]
-getCombibation([1, 2, 4, 8], 2, ret, vis, 0);
-console.log(ret)
+//console.log(readBinaryWatch(0));
