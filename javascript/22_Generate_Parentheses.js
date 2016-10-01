82d13024ed8ec8d5cb095375866574bae18311ee
samael <samael.65535@gmail.com>
2016-10-01 23:29:57 +0800

    22

---
+++ javascript/22_Generate_Parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var ret;
+var generateParenthesis = function(n) {
+	var str = "";
+    ret = [];
+    getParenthesis(str, 0, 0, n);
+    return ret;    
+};
+
+var getParenthesis = function(str, l, r, n) {
+    if (l==r && l==n){
+        ret.push(str)
+    } else {
+         if (l<n) {
+                getParenthesis(str+"(", l+1, r, n);
+            }
+            if (l>r) {
+                getParenthesis(str+")", l, r+1, n);
+            }
+    }
+}
+
+//generateParenthesis(3);
