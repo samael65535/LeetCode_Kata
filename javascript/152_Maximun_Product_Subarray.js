6ab3ce247f1511b43673b20d0a3e68607773460d
samael <samael.65535@gmail.com>
2016-10-04 12:03:53 +0800

    refactor 152

--- javascript/152_Maximun_Product_Subarray.js
+++ javascript/152_Maximun_Product_Subarray.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-	var arr1 = [nums[0]];
-	var arr2 = [nums[0]];
-	var max = nums[0];
+	var ret = nums[0];
+	
+	var max = nums[0], min = nums[0];
 	for (var i = 1; i < nums.length; i++) {
-		var p1 = Math.max(nums[i] * arr1[i-1], nums[i] * arr2[i-1]);
-		var p2 = Math.min(nums[i] * arr1[i-1], nums[i] * arr2[i-1]);
+		var p1 = Math.max(nums[i] * max, nums[i] * min);
+		var p2 = Math.min(nums[i] * max, nums[i] * min);
 		
-		arr1[i] = Math.max(p1, nums[i]);
-		arr2[i] = Math.min(p2, nums[i]);
+		max = Math.max(p1, nums[i]);
+		min = Math.min(p2, nums[i]);
 
-		max = Math.max(max, arr1[i]);
+		ret = Math.max(max, ret);
 	}
-	return max;
+	return ret;
 };
-
-maxProduct([-2]);
