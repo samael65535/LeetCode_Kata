45a4872ff1816ba13a0f75597744b6b573d058cc
samael <samael.65535@gmail.com>
2016-10-17 22:38:05 +0800

    80

--- javascript/80_Remove_Duplicates_from_Sorted_Array_II.js
+++ javascript/80_Remove_Duplicates_from_Sorted_Array_II.js
@@ -5,7 +5,7 @@
 var removeDuplicates = function(nums) {
 	if (nums.length < 3) return nums.length;
 	var idx = 2;
-	for (var i = 2; i < n; i++) {
+	for (var i = 2; i < nums.length; i++) {
 		if (nums[i] != nums[idx - 2]) {
 			nums[idx++] = nums[i];
 		}
