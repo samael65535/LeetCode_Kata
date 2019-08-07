50dce7b6625ad5efc00b23cfb028522427c29446
samael <samael.65535@gmail.com>
2019-08-07 10:24:07 +0800

    14

--- javascript/14.longest-common-prefix.js
+++ javascript/14.longest-common-prefix.js
@@ -3,5 +3,27 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
+    let ans = ""
+	let row = strs.length
+	if (row == 0) return  ans
+	let col = strs[0].length
+	let flag = true
+	for (let c = 0; c < col; c++) {
+		let cur = strs[0][c]
+		for (let r = 1; r < row; r++) {
+			flag = flag && strs[r][c] == cur
+			if (flag == false)  {
+				return ans
+			}
+		}
+		if (flag) {
+			ans+=cur
+		}
+	}
+	return ans
 };
+
+//
+// console.log(longestCommonPrefix(["flower","flow","flight"]))
+// console.log(longestCommonPrefix( ["dog","racecar","car"]))
+// console.log(longestCommonPrefix( ['aa']))
