fac1ad77d873035cb002cf62e059ac18327321a9
samael <samael.65535@gmail.com>
2019-08-09 09:47:23 +0800

    206

---
+++ javascript/206.reverse-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function(head) {
+	if (head == null) return head
+	let prev = null
+	let cur = head
+	while(cur != null) {
+		let tmp = cur.next
+		cur.next = prev
+		prev = cur
+		cur = tmp
+	}
+	return prev
+};
