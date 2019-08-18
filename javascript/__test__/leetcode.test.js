552ef71c36ccb63df728dbf903c5ec2ff1cc0182
samael <samael.65535@gmail.com>
2019-08-18 19:05:19 +0800

    98

--- javascript/__test__/leetcode.test.js
+++ javascript/__test__/leetcode.test.js
@@ -1,13 +1,4 @@
 let leetcode = require('leetcode')
-test('104 test', () => {
-	let maxDepth = require('../104.maximum-depth-of-binary-tree')
-	expect(1).toBe(1)
-	expect(
-		maxDepth(leetcode.Tree.create([3,9,20,null,null,15,7]))
-	).toBe(3)
-})
-
-
 test('98 test', () => {
 	let isValidBST = require('../98.validate-binary-search-tree')
 	let tree = leetcode.Tree.create;
@@ -19,3 +10,13 @@ test('98 test', () => {
 	expect(isValidBST(tree([8, 3, 10, 1, 6, null,  14, null,  null,  4, 7, 13, null]))).toBeTruthy()
 	expect(isValidBST(tree([10,5,15,null,null,6,20]))).toBeFalsy()
 })
+
+test('104 test', () => {
+	let maxDepth = require('../104.maximum-depth-of-binary-tree')
+	expect(1).toBe(1)
+	expect(
+		maxDepth(leetcode.Tree.create([3,9,20,null,null,15,7]))
+	).toBe(3)
+})
+
+
