fc0bde9a9fbccb0f42457b274b61d259561eed05
samael <samael.65535@gmail.com>
2019-09-25 13:34:22 +0800

    103

--- javascript/103.binary-tree-zigzag-level-order-traversal.js
+++ javascript/103.binary-tree-zigzag-level-order-traversal.js
@@ -9,12 +9,37 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var zigzagLevelOrder = function(root) {
-	return [
-		[3],
-		[20,9],
-		[15,7]
-	]
-
+var zigzagLevelOrder = function (root) {
+	if (root == null) return []
+	// 层次遍历然后翻转
+	let list = [[root]]
+	let row = 0
+	while(1) {
+		let curRow = list[row]
+		if (curRow) {
+			curRow.forEach((node, idx) => {
+				if (node.left) {
+					if (!list[row+1]) {
+						list.push([])
+					}
+					list[row+1].push(node.left)
+				}
+				if (node.right) {
+					if (!list[row+1]) {
+						list.push([])
+					}
+					list[row+1].push(node.right)
+				}
+			})
+			row++
+		} else {
+			break
+		}
+	}
+	return list.map((l, i) => {
+		if(i % 2)
+			return l.map((a) => a.val).reverse()
+		return l.map((a) => a.val)
+	})
 };
 module.exports = zigzagLevelOrder
