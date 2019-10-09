cd166e0e6271ef3163053852d058e140734eb485
samael <samael.65535@gmail.com>
2019-10-09 12:14:06 +0800

    1.

---
+++ javascript/1.two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+	let map = {}
+	for (let i = 0; i < nums.length; i++) {
+		if(map[target - nums[i]] != undefined) {
+			return [i, map[target - nums[i]]]
+		}
+		map[nums[i]] = i
+	}
+};
+
+console.log(twoSum([3, 5, 1], 6))
