6ff77d2626d052e41ca8ef26eab2e8ba4f3f3bb9
samael <samael.65535@gmail.com>
2019-10-11 16:56:34 +0800

    297

--- javascript/59_Spiral_Matrix_II.js
+++ javascript/59_Spiral_Matrix_II.js
@@ -2,38 +2,38 @@
  * @param {number} n
  * @return {number[][]}
  */
-var generateMatrix = function(n) {
+var generateMatrix = function (n) {
 	if (n == 1) return [[1]];
 	var ret = new Array(n);
 	var currentDirection = 0;
-	for (var i=0; i<n; i++) {
+	for (var i = 0; i < n; i++) {
 		ret[i] = new Array(n);
-		for (var j=0; j<n; j++) {
+		for (var j = 0; j < n; j++) {
 			ret[i][j] = 0;
 		}
 	}
 
 	var i = 0, j = 0, val = 1;
-	while(val <= n*n) {
-	
-		while(ret[i][j] == 0 && j < n) { // right
+	while (val <= n * n) {
+
+		while (ret[i][j] == 0 && j < n) { // right
 			ret[i][j++] = val++;
 		}
 		i += 1;
 		j -= 1;
-		while(i < n && ret[i][j] == 0 ) { // down
+		while (i < n && ret[i][j] == 0) { // down
 			ret[i++][j] = val++;
 		}
-	
+
 		j -= 1;
 		i -= 1;
-		while(j >= 0 && ret[i][j] == 0 ) { // left
+		while (j >= 0 && ret[i][j] == 0) { // left
 			ret[i][j--] = val++;
 		}
-	
+
 		i -= 1;
 		j += 1;
-		while(j <= n && ret[i][j] == 0) { // up
+		while (j <= n && ret[i][j] == 0) { // up
 			ret[i--][j] = val++;
 		}
 		i += 1;
